c333d1cdc15b42899060513693735dd9345be6ee
Galtarus <60255433+Galtarus@users.noreply.github.com>
2023-07-22 13:54:57 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,56 @@
+// This is the URL to your webhook. Replace this with your own.
+const WEBHOOK_URL = "https://discord.com/api/webhooks/your_webhook_here";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "sendToDiscord",
+    title: "Send to Discord",
+    contexts: ["selection", "image", "video"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "sendToDiscord") {
+    let formData = new FormData();
+    if (info.srcUrl) {
+      let response = await fetch(info.srcUrl);
+      let blob = await response.blob();
+      let dataUrl = await blobToDataURL(blob);
+      let splitUrl = dataUrl.split(",");
+      let base64Data = splitUrl[1];
+      let format = splitUrl[0].match(/(.*)\/(.*);base64/)[2];
+      
+      let fileBlob = base64ToBlob(base64Data, `${format}`);
+      formData.append("file", fileBlob, `file.${format}`);
+    } else if (info.selectionText) {
+      formData.append("content", info.selectionText);
+    } else {
+      console.log("No media URL or highlighted text found.");
+      return;
+    }
+    
+    fetch(WEBHOOK_URL, {
+      method: 'POST',
+      body: formData,
+    }).catch(error => console.error('Error:', error));
+  }
+});
+
+function blobToDataURL(blob) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = (e) => resolve(e.target.result);
+    reader.readAsDataURL(blob);
+  });
+}
+
+function base64ToBlob(base64, mimeType) {
+  var byteString = atob(base64);
+  var arrayBuffer = new ArrayBuffer(byteString.length);
+  var int8Array = new Uint8Array(arrayBuffer);
+  for (var i = 0; i < byteString.length; i++) {
+    int8Array[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([int8Array], {type: mimeType});  
+}
